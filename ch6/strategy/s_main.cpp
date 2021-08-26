692d480f133eb88ed10fbb4a4201fd4170cc8861
liu15388282385 <1436255653@qq.com>
2021-08-26 14:40:59 +0800

    master

--- ch6/strategy/s_main.cpp
+++ ch6/strategy/s_main.cpp
@@ -14,13 +14,137 @@ int main() {
 }
 
 void prog1() {
+    const int pos = 8;
 
+	num_sequence<Fibonacci> fib;
+	display(cout, fib, pos);
+
+	num_sequence<Pell> pell;
+	display(cout, pell, pos);
+
+	num_sequence<Lucas> lucas;
+	display(cout, lucas, pos);
+
+	num_sequence<Triangular> trian;
+	display(cout, trian, pos);
+
+	num_sequence<Square> square;
+	display(cout, square, pos);
+
+	num_sequence<Pentagonal> penta;
+	display(cout, penta, pos);
 }
 
 void prog2() {
-    
+    num_sequence<Fibonacci> fib(1, 8);
+	cout << "fib: " << fib.what_am_i() << endl;
+	num_sequence<Pell> pell(4, 6);
+	num_sequence<Lucas> lucas(7, 10);
+	num_sequence<Triangular> trian(1, 12);
+	num_sequence<Square> square(6, 6);
+	num_sequence<Pentagonal> penta(1, 8);
+
+	cout << "fib: " << fib << '\n'
+		 << "pell: " << pell << '\n'
+		 << "lucas: " << lucas << '\n'
+		 << "trian: " << trian << '\n'
+		 << "square: " << square << '\n'
+		 << "penta: " << penta << endl;
+}
+
+template <typename seq_type>
+void prog_3(seq_type ns, int elem_values) {
+	int elem_val = 0;
+	const int pos = 8;
+	const int find_pos = 14;
+
+	elem_val = ns.elem(pos);
+	display(cout, ns, pos, elem_val);
+	cout << ns.what_am_i() << " : ( "
+		 << ns.beg_pos() << ", "
+		 << ns.length() << " ) : "
+		 << ns;
+	cout << endl;
+
+	ns.set_length(12);
+	cout << ns << endl;
+
+	cout << "num_sequence::is_elem() ";
+	if (!ns.is_elem(elem_val) ||
+		ns.is_elem(2 * elem_val))
+		cout << "failed\n";
+	else
+		cout << "ok!\n";
+
+	cout << "copy constructor and equality ";
+	seq_type ns2 = ns;
+	if (ns != ns2)
+		cout << "failed!\n";
+	else
+		cout << "ok!\n";
+
+	ns2.set_position(5);
+
+	int posit = ns.pos_elem(elem_val);
+	if (ns.pos_elem(elem_val) != pos)
+		cout << "pos_elem() failed : " << posit << "\n";
+	else
+		cout << "pos_elem() -- ok: " << posit << "\n";
+
+	posit = ns.pos_elem(elem_values);
+	if (posit != find_pos)
+		cout << "pos_elem(): not found but grown: failed: "
+			 << posit << " correct: " << find_pos << "\n";
+	else
+		cout << "pos_elem(): not found but grown: ok\n";
+
+	cout << "about to print final ns vector: \n";
+	ns.print();
+	cout << endl;
+
+	cout << "ns2: should begin at position 5:\n"
+		 << ns2 << endl;
 }
 
 void prog3() {
-    
+    int elem_values[] = {377, 80782, 843, 105, 196, 287};
+
+	// for ( int ix = 0; ix < 6; ++ix ) // test all
+	for (int ix = 0; ix < 1; ++ix) {  // test 1 at time
+		switch (ix) {
+		case 0: {
+			num_sequence<Fibonacci> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		case 1: {
+			num_sequence<Pell> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		case 2: {
+			num_sequence<Lucas> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		case 3:
+		{
+			num_sequence<Triangular> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		case 4: {
+			num_sequence<Square> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		case 5: {
+			num_sequence<Pentagonal> ns(1, 8);
+			prog_3(ns, elem_values[ix]);
+			break;
+		}
+		default:
+			return;
+		}
+	}
 }
--- ch6/strategy/s_main.h
+++ ch6/strategy/s_main.h
@@ -27,11 +27,11 @@ template<typename seq_type>
 class num_sequence {
 public:
     typedef vector<uint>::iterator iterator;
-    num_sequence(unit len = 1, uint beg_pos = 1);
+    num_sequence(unit beg_pos = 1, uint len = 1);
 
     unit elem(uint pos) const;
-    bool is_elem(uint) const;
-    uint pos_elem(unit) const;
+    bool is_elem(uint elem) const;
+    uint pos_elem(unit elem) const;
 
     const char *what_am_i() const { return seq_type::what_am_i(); }
     static uint max_elems() { return _max_elems; }
@@ -69,9 +69,10 @@ ostream &operator<<(ostream &os, const num_sequence<seq_type> &ns) {
     return ns.print(os);
 }
 
+// constructor
 template<typename seq_type>
 num_sequence<seq_type>::
-num_sequence(uint len, uint beg_pos) : _pelems() {
+num_sequence(uint beg_pos, uint len) : _pelems(seq_type::elemset()) {
     set_position(beg_pos);
     set_length(len);
 }
@@ -121,7 +122,7 @@ set_length(uint len) {
 template <typename seq_type>
 inline uint num_sequence<seq_type>::
 elem(uint pos) const {
-    return (check_integrity(pos, _pelems->size()))
+    return (!check_integrity(pos, _pelems->size()))
             ? 0
             : (*_pelems)[pos - 1];
 }
@@ -155,10 +156,276 @@ print(ostream &os) const {
     os << "("<< _beg_pos <<", "<< _length <<")";
 
     while (elem_pos < end_pos)
-        os << *_pelems[elem_pos++] << " ";
+        os << (*_pelems)[elem_pos++] << " ";
     os << endl;
 
     return os;  
+}
+
+// is_elem() returns true if the element passed in a valid element
+// in the object's sequence. For example, if the object represents
+// a fibonacci sequence { 1,1, 2, 3, 5, 8, 13, 21, 34, 45}, beginning
+// as position 3 for a length of 2, then
+//    Obj.is_elem( 1 ); // false
+//    Obj.is_elem( 3 ); // true
+//    Obj.is_elem( 5 ); // false
+//    Obj.is_elem( 4 ); // false
+template <typename seq_type>
+inline bool num_sequence<seq_type>::
+is_elem(uint elem) const {
+    return (!check_integrity(_beg_pos, _length))
+           ? false
+           : binary_search(begin(), end(), elem);
+}
+
+// pos_elem() returns the position of an element within the sequence
+// independent of what the object's position and length. The object
+// simply is used to identify the sequence for which to return a position.
+// If the element value is invalid, return 0. For example, if the object
+// represents a fibonacci sequence, then
+//     Obj.pos_elem( 1 ); // returns 1 - ignore duplicate
+//     Obj.pos_elem( 32 ); // returns 9
+//     Obj.pos_elem( very-large-instance ); // your choice
+//     Obj.pos_elem( 4 ); // return 0
+template <typename seq_type>
+uint num_sequence<seq_type>::
+pos_elem(uint elem) const {
+    cout << "pos_elem( " << elem << " )\n";
+
+    if((*_pelems)[_pelems->size() - 1] < elem)
+        return calc_pos(elem);
+    
+    iterator iter = find(_pelems->begin(), _pelems->end(), elem);
+    return (iter == _pelems->end())
+           ? 0
+           : distance(_pelems->begin(), iter) + 1;// distance()函数用于计算两个迭代器范围内的元素个数
+}
+
+template <typename seq_type>
+uint num_sequence<seq_type>::
+calc_pos (uint elem) const {
+    int pos = _pelems->size() - 1;
+    cout << "calc_pos invoked()!: elem: " << elem
+		 << " pos: " << pos
+		 << " at: " << (*_pelems)[pos] << endl;
+    
+    while (pos < _max_elems && (*_pelems)[pos] < elem) {
+        _ns.gen_elems(++pos);
+        cout << "pos: " << pos
+             << " at: " << (*_pelems)[pos]
+             << endl;
+    }
+
+    return (pos < _max_elems) && ((*_pelems)[pos] == elem)
+           ? pos + 1
+           : 0;
+}
+
+class Fibonacci {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Fibonacci"; }
+
+protected:
+    static vector<uint> _elems;
+};
+
+class Pell {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Pell"; }
+    
+protected:
+    static vector<uint> _elems;
+};
+
+class Lucas {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Lucas"; }
+    
+protected:
+    static vector<uint> _elems;
+};
+
+class Triangular {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Triangular"; }
+    
+protected:
+    static vector<uint> _elems;
+};
+
+class Square {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Square"; }
+    
+protected:
+    static vector<uint> _elems;
+};
+
+class Pentagonal {
+public:
+    void gen_elems(uint pos) const;
+    static vector<uint> *elemset() { return &_elems; }
+    static const char *what_am_i() { return "Pentagonal"; }
+    
+protected:
+    static vector<uint> _elems;
+};
+
+vector<uint> Fibonacci::_elems;
+vector<uint> Pell::_elems;
+vector<uint> Lucas::_elems;
+vector<uint> Triangular::_elems;
+vector<uint> Square::_elems;
+vector<uint> Pentagonal::_elems;
+
+template<typename seq_type>
+inline void display(ostream &os, const num_sequence<seq_type> &ns, 
+                    uint pos) {
+    os << "The element at position "
+	   << pos << " for the "
+	   << ns.what_am_i() << " sequence is "
+	   << ns.elem(pos) << endl;
+}
+
+template<typename seq_type>
+inline void display(ostream &os, const num_sequence<seq_type> &ns, 
+                    uint pos, uint elem_val) {
+    os << "The element at position " << pos
+	   << " for the "
+	   << ns.what_am_i()
+	   << " sequence is " << elem_val << endl;
+}
+
+void Fibonacci::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Fibonacci>::max_elems())
+		return;
+
+	if (_elems.empty()) {
+		_elems.push_back(1);
+		_elems.push_back(1);
+	}
+
+	if (_elems.size() <= pos) {
+		int ix = _elems.size();
+		int n_2 = _elems[ix - 2],
+			n_1 = _elems[ix - 1];
+
+		int elem;
+		for (; ix <= pos; ++ix) {
+			elem = n_2 + n_1;
+			// cout << "gen_elems: " << elem << endl;
+			_elems.push_back(elem);
+			n_2 = n_1;
+			n_1 = elem;
+		}
+	}
+}
+
+void Pell::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Pell>::max_elems())
+		return;
+
+	if (_elems.empty()) {
+		_elems.push_back(1);
+		_elems.push_back(2);
+	}
+
+	if (_elems.size() <= pos) {
+		int ix = _elems.size();
+		int n_2 = _elems[ix - 2],
+			n_1 = _elems[ix - 1];
+
+		int elem;
+		for (; ix <= pos; ++ix) {
+			elem = n_2 + 2 * n_1;
+			_elems.push_back(elem);
+			n_2 = n_1;
+			n_1 = elem;
+		}
+	}
+}
+
+void Lucas::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Lucas>::max_elems())
+		return;
+
+	if (_elems.empty()) {
+		_elems.push_back(1);
+		_elems.push_back(3);
+	}
+
+	if (_elems.size() <= pos) {
+		int ix = _elems.size();
+		int n_2 = _elems[ix - 2],
+			n_1 = _elems[ix - 1];
+
+		int elem;
+		for (; ix <= pos; ++ix) {
+			elem = n_2 + n_1;
+			_elems.push_back(elem);
+			n_2 = n_1;
+			n_1 = elem;
+		}
+	}
+}
+
+void Triangular::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Triangular>::max_elems())
+		return;
+
+	if (_elems.size() <= pos) {
+		int end_pos = pos + 1;
+		int ix = _elems.size() + 1;
+
+		for (; ix <= end_pos; ++ix)
+			_elems.push_back(ix * (ix + 1) / 2);
+	}
+}
+
+void Square::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Square>::max_elems())
+		return;
+
+	if (_elems.size() <= pos) {
+		int end_pos = pos + 1;
+		int ix = _elems.size() + 1;
+		for (; ix <= end_pos; ++ix)
+			_elems.push_back(ix * ix);
+	}
+}
+
+void Pentagonal::
+gen_elems(uint pos) const {
+	if (pos <= 0 ||
+		pos > num_sequence<Pentagonal>::max_elems())
+		return;
+
+	if (_elems.size() <= pos) {
+		int end_pos = pos + 1;
+		int ix = _elems.size() + 1;
+		for (; ix <= end_pos; ++ix)
+			_elems.push_back(ix * (3 * ix - 1) / 2);
+	}
 }
 
 // 测试函数

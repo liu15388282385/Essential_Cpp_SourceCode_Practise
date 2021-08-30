3abd825ba1c7298297a0b95f889866a1759a7b84
liu15388282385 <1436255653@qq.com>
2021-08-30 15:23:14 +0800

    master

--- chapter4/ch4/ch4.h
+++ chapter4/ch4/ch4.h
@@ -251,33 +251,33 @@ private:
     int _max;
 };
 
-// example
-extern void log_message(const char*);
-extern string err_message[];
-extern ostream log_file;
+// // example
+// extern void log_message(const char*);
+// extern string err_message[];
+// extern ostream log_file;
 
-bool has_elem(Triangular_iterator first, 
-              Triangular_iterator last, int elem) {
+// bool has_elem(Triangular_iterator first, 
+//               Triangular_iterator last, int elem) {
 
-    bool status = true;
+//     bool status = true;
 
-    try {
-        while (first != last) {
-            if (*first == elem)
-                return status;
-            ++first;
-        }   
-    }
-    // try块内的程序代码执行时，如果有任何异常抛出，
-    // 我们之前捕获其中类型为iterator_overflow的异常
-    catch(iterator_overflow &iof) {
-        // log_message(iof.what_happened());
-        log_message("check if iterators address same container");
-    }
+//     try {
+//         while (first != last) {
+//             if (*first == elem)
+//                 return status;
+//             ++first;
+//         }   
+//     }
+//     // try块内的程序代码执行时，如果有任何异常抛出，
+//     // 我们之前捕获其中类型为iterator_overflow的异常
+//     catch(iterator_overflow &iof) {
+//         // log_message(iof.what_happened());
+//         log_message("check if iterators address same container");
+//     }
 
-    status = false;
-    return status;
-}
+//     status = false;
+//     return status;
+// }
 
 //检查下标是否合法
 inline void 

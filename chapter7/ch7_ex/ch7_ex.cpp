76effe7bd8952b88e167f3b5be79bed42a27a9c8
liu15388282385 <1436255653@qq.com>
2021-09-05 17:08:51 +0800

    master

--- chapter7/ch7_ex/ch7_ex.cpp
+++ chapter7/ch7_ex/ch7_ex.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+typedef bool noMem;
+
 int *alloc_and_init(string file_name);
 
 int *allocate_array(int);
@@ -23,7 +25,7 @@ int main () {
     return 0;
 }
 
-// 练习 7-1
+// 练习 7-1、7-2
 int *alloc_and_init(string file_name) {
 
     // ifstream infile(file_name); // 类型不匹配，ifstream接受const char *类型
@@ -31,28 +33,47 @@ int *alloc_and_init(string file_name) {
     // 定义infile应该检查它是否成功打开
     if (!infile) {
         // 打开文件失败....
+        return 0;
     }
     int elem_cnt;
     infile >> elem_cnt;
     // 读取数据后应该检查是否读取成功
     if (!infile) {
         // 读取数据失败....
+        return 0;
     }
-    int *pi = allocate_array(elem_cnt);
-    // 处理指针前，必须确认是否指向实际存在的对象
-    if (!pi) {
-        // allocate_array()没有分配到内存
+    int *pi;
+    try {
+        pi = allocate_array(elem_cnt);  // (1)
+        // 处理指针前，必须确认是否指向实际存在的对象
+        if (!pi) {
+            // allocate_array()没有分配到内存
+        }
+        int elem;
+        int index = 0;
+        while (infile >> elem)
+            pi[index++] = elem;
+
+        sort_array(pi, elem_cnt);   // (2)
+        register_data(pi);          // (3)
     }
-    int elem;
-    int index = 0;
-    while (infile >> elem)
-        pi[index++] = elem;
+    catch(const noMem &memFail) {   //异常情况(1)
+        cerr << "alloc_and_init(): allocate_array() failure!\n"
+             << memFail << endl;
+        return 0;
+    }
+    catch(int &sortFail) {          //异常情况(2)
+        cerr << "alloc_and_init(): sort_array() failure!\n"
+             << "thrown integer value: " << sortFail; 
+        return 0;
+    }
+    catch(string &registerFail) {   //异常情况(3)
+        cerr << "alloc_and_init():register_data failure!\n"
+			 << "thrown integer value: " << registerFail << endl;
+		return 0;
+    }
 
-    sort_array(pi, elem_cnt);
-
-    register_data(pi);
-
-    return pi;
+    return pi; // 如果没有任何异常，才会执行到这里
 
 }
 
